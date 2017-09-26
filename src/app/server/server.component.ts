2320290a35ed95e1de536e2c35725b2df8b6b3da
Matheus Guedes <matheushg67@hotmail.com>
2017-09-26 17:51:14 -0300

    String Interpolation
    Testing how string interpolation works in Angular

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,5 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './server.component.html'
 })
 export class ServerComponent {
+  serverId = 10;
+  serverStatus: string =  'offline'; //could be without the : string
 
+  getServerStatus(){
+    return this.serverStatus;
+  }
 }
