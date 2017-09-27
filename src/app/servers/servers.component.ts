c25088d5f2f8c87f92a4d0fd725bf32216f32a1a
Matheus Guedes <matheushg67@hotmail.com>
2017-09-27 11:36:22 -0300

    Two-Way-Databinding Criado um outro input usando [(ngModel)] para ver como two-way databinding funciona

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,7 +13,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created!';
-  serverName = '';
+  serverName = 'Testserver';
 
   constructor() { 
     setTimeout(() => {
@@ -30,7 +30,6 @@ export class ServersComponent implements OnInit {
 
   onUpdateServerName(event: Event) {
     this.serverName = (<HTMLInputElement>event.target).value;
-
   }
 
 }
